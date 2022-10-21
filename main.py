494028718152a9d026ecca60fe69b673aa73356a
MOB_master <lera061009@gmail.com>
2022-10-21 21:29:41 +0300

    home 1

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class Student:
     print('hello teacher')
     def __init__(self):
         self.height = 175
-        self.age = 13
+        self.age = 14
 
 student_mark = Student()
+print(student_mark.height)
+print(student_mark.age)
